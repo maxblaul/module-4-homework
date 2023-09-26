f296c7b441f854f9b07d330a909cd5da4e675f07
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-09-25 22:28:57 -0500

    worked on hw 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 var wronganswerButton="wronganswer"
 var correctanswerButton="correctanswer"
+var startButton=document.querySelector(".start-button")
+
+startButton.addEventListener("click", function () {
+    var container=document.querySelector(".container")
+    container.classList.remove("hide")
+    startButton.classList.add("hide")
+    startCountdown(); 
+})
 // set interval time to 1 minute
 let time = 60;
 // timer needs to update after every 1 second
 function updateTimerDisplay() {
     var timerElement = document.getElementById('timer');
-    timerElement.textContent = '${time} seconds';
+    timerElement.textContent = `${time} seconds`;
 }
 // countdown needs to initialize
 function startCountdown() {
@@ -25,6 +33,5 @@ document.getElementById('wronganswerButton').addEventListener('click', function 
     time -= 15; 
     updateTimerDisplay();
 });
-startCountdown();
 
-function 
+
